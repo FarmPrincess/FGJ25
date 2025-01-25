f1b6e4dd81aaa481fc36b37aed053f18bc6739e9
ajanvarays <camilove150900@gmail.com>
2025-01-25 02:31:47 +0200

    Added friend that tracks players movement above

---
+++ FGJ25/Assets/FriendController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendController : MonoBehaviour
+{
+    private Transform _player;
+
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        var step = .6f * Time.deltaTime;
+        var target = new Vector3(_player.position.x, _player.position.y + 3, _player.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+}
