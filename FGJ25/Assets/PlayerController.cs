d719ac737f9d3ed4a62271e4cb66e9856a6bf385
ajanvarays <camilove150900@gmail.com>
2025-01-26 03:05:15 +0200

    Added some speechbubble asset things for tomorrow

--- FGJ25/Assets/PlayerController.cs
+++ FGJ25/Assets/PlayerController.cs
@@ -29,7 +29,6 @@ public class PlayerController : MonoBehaviour
 
         controls.Player.StopFriend.started += _ => testFriend.Stop();
 
-
     }
 
     private void OnEnable() => controls.Enable();
---
+++ FGJ25/Assets/SpeechBubbleBehaviour.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SpeechBubbleBehaviour : MonoBehaviour
+{
+    // a few bubble ideas
+    public SpeechBubbleBehaviour Tutorial;
+    public SpeechBubbleBehaviour Turtle;
+    public SpeechBubbleBehaviour Jelly;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // after 3 sec show tutorial bubble
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // add void collision shit
+    // when player jumps on top of fish, they say something
+
+    // player controls?
+    // on collision, call for Instantiate()
+
+    // every fish has their own speechbubble
+    // every bubble has their own static position and sprite
+    
+}
